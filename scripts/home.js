3e1ed3339bf19b0ea354658dc1e76ce7d6e3481f
Filipe Assis <filipeassisr@gmail.com>
2025-06-16 17:01:50 -0300

    inicio sections

--- scripts/home.js
+++ scripts/home.js
@@ -1,13 +1,28 @@
-const titulo = document.getElementById("titulo-hero");
+const tituloHero = document.getElementById("titulo-hero");
 
 const palavras = ['economiza', 'agiliza', 'escala', 'muda tudo']
 
 let indice = 0;
 
 function alternarPalavra() {
-    
+
          indice = (indice + 1) % palavras.length;
 
-        titulo.innerHTML = `Informática com simplicidade, <span> ${palavras[indice]}! </span>`; 
+        tituloHero.innerHTML = ` <span> ${palavras[indice]}! </span>`; 
 
         } setInterval(alternarPalavra, 2000); 
+
+
+
+function upperH2() {
+    // Seleciona todos os elementos h2 na página
+    const h2Elements = document.querySelectorAll('h2');
+    
+    // Itera sobre todos os h2 e aplica o toUpperCase
+    h2Elements.forEach(h2 => {
+        h2.innerText = h2.innerText.toUpperCase();
+    });
+}
+
+// Chama a função
+upperH2();
